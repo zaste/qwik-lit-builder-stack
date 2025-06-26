e1f0a80fbdbe62209f252240c30f4081762c0ad2
Mr.Gr0wth <46140662+zaste@users.noreply.github.com>
2025-06-26 23:15:54 +0200

    feat: Add Cloudflare Pages entry point

---
+++ src/entry.cloudflare-pages.tsx
@@ -0,0 +1,17 @@
+import { createQwikCity } from '@builder.io/qwik-city/middleware/cloudflare-pages';
+import qwikCityPlan from '@qwik-city-plan';
+import { manifest } from '@qwik-client-manifest';
+import render from './entry.ssr';
+
+const fetch = createQwikCity({ render, qwikCityPlan, manifest });
+
+export default {
+  fetch,
+} satisfies ExportedHandler<{
+  KV: KVNamespace;
+  R2: R2Bucket;
+  SUPABASE_URL: string;
+  SUPABASE_ANON_KEY: string;
+  SUPABASE_SERVICE_KEY?: string;
+  BUILDER_PUBLIC_KEY: string;
+}>;
