bb2ac8529c89708e75769b96fcf1f0cb890b30fb
Mr.Gr0wth <46140662+zaste@users.noreply.github.com>
2025-06-26 23:17:29 +0200

    feat: Add Web Test Runner configuration

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,25 +1,30 @@
 import { playwrightLauncher } from '@web/test-runner-playwright';
+import { esbuildPlugin } from '@web/dev-server-esbuild';
 
 export default {
-  files: 'src/**/*.test.ts',
+  files: 'src/design-system/**/*.test.ts',
   nodeResolve: true,
-  
+  plugins: [
+    esbuildPlugin({
+      ts: true,
+      tsx: true,
+      target: 'auto',
+    }),
+  ],
   browsers: [
     playwrightLauncher({ product: 'chromium' }),
     playwrightLauncher({ product: 'firefox' }),
     playwrightLauncher({ product: 'webkit' }),
   ],
-
   testFramework: {
     config: {
-      ui: 'bdd',
       timeout: 3000,
+      retries: 1,
     },
   },
-
   coverageConfig: {
     include: ['src/design-system/**/*.ts'],
-    exclude: ['**/*.test.ts', '**/*.stories.ts'],
+    exclude: ['**/*.test.ts', '**/*.spec.ts', '**/node_modules/**'],
     threshold: {
       statements: 80,
       branches: 80,
